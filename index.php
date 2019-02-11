e4a16c131fcea0589b4c7f6aa1702c6a814ebb1d
Julio Quevedo <juliovquevedo@gmail.com>
2019-02-11 14:20:30 -0500

    Simplified the code first part of form works

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-$buyOneItemPrice = filter_input(INPUT_POST, 'buyoneprice');
-$buyOneItemPrice2 = filter_input(INPUT_POST, 'buysecondprice');
-$buyOneItems = filter_input(INPUT_POST, 'itembought');
-$buyOneItemDeal = filter_input(INPUT_POST, 'getthisdeal');
-$buyOneDiscount = filter_input(INPUT_POST, 'discount');
-$firstDeal = 100 - ($buyOneItemPrice * $buyOneItems + ($buyOneItemPrice2 * $buyOneItemDeal * $buyOneDiscount / 100)) / ($buyOneItemPrice * $buyOneItems + $buyOneItemPrice2 * $buyOneItemDeal) * 100;
-$buyOneSavings = ($buyOneItemPrice * $buyOneItems + $buyOneItemPrice2 * $buyOneItemDeal) * $firstDeal / 100;
+$d1Item1Price = filter_input(INPUT_POST, 'buyoneprice');
+$d1Item2Price = filter_input(INPUT_POST, 'buysecondprice');
+$d1Item1 = filter_input(INPUT_POST, 'itembought');
+$d1Item2 = filter_input(INPUT_POST, 'getthisdeal');
+$d1Discount = filter_input(INPUT_POST, 'discount');
+
+$d1TotalPrice = $d1Item1Price * $d1Item1 + $d1Item2Price * $d1Item2;
+$d1DiscountAmount = $d1Item2Price * $d1Item2 * $d1Discount / 100;
+$d1Percent = $d1DiscountAmount / $d1TotalPrice * 100;
+$d1PercentF = number_format($d1Percent, 2);
 ?>
 
 <!doctype html>
@@ -15,11 +18,8 @@ $buyOneSavings = ($buyOneItemPrice * $buyOneItems + $buyOneItemPrice2 * $buyOneI
         <title>Compare Deals</title>
     </head>
     <body>
-        <p><?php echo $buyOneItemPrice ?></p>
-        <p><?php echo $buyOneItems ?></p>
-        <p><?php echo $buyOneItemDeal ?></p>
-        <p><?php echo $buyOneDiscount ?></p>
-        <p><?php echo $firstDeal . " percent off."?></p>
-        <p><?php echo "You save " . $buyOneSavings ?></p>
+        <p><?php echo "Total Price before discount: " . $d1TotalPrice ?></p>
+        <p><?php echo "Discount Amount: " . $d1DiscountAmount ?></p>
+        <p><?php echo "You got " . $d1PercentF . " percent off on this deal."?></p>
     </body>
 </html>
