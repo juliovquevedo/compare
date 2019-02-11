7b66a858fdb239ce1eac216e7753b7bcbd7e2138
Julio Quevedo <juliovquevedo@gmail.com>
2019-02-11 14:51:30 -0500

    Price for each item now showing

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ $d1DiscountAmount = $d1Item2Price * $d1Item2 * $d1Discount / 100;
 $d1DiscountAmountF = number_format($d1DiscountAmount, 2);
 $d1Percent = $d1DiscountAmount / $d1TotalPrice * 100;
 $d1PercentF = number_format($d1Percent, 2);
+$d1Each = ($d1TotalPrice - $d1DiscountAmount) / ($d1Item1 + $d1Item2);
+$d1EachF = number_format($d1Each, 2);
 
 
 $d2TotalPrice = filter_input(INPUT_POST, 'd2price');
@@ -19,6 +21,8 @@ $d2Percent = filter_input(INPUT_POST, 'd2percent');
 $d2PercentF = number_format($d2Percent, 2);
 $d2DiscountAmount = $d2TotalPrice * $d2Percent / 100;
 $d2DiscountAmountF = number_format($d2DiscountAmount, 2);
+$d2Each = ($d2TotalPrice - $d2DiscountAmount) / ($d1Item1 + $d1Item2);
+$d2EachF = number_format($d2Each, 2);
 ?>
 
 <!doctype html>
@@ -32,12 +36,14 @@ $d2DiscountAmountF = number_format($d2DiscountAmount, 2);
         <h3>First Deal</h3>
         <p><?php echo "Total Price before discount: $" . $d1TotalPriceF ?></p>
         <p><?php echo "Discount Amount: $" . $d1DiscountAmountF ?></p>
+        <p><?php echo "You paid $" . $d1EachF . " for each item." ?></p>
         <p><?php echo "You got " . $d1PercentF . "% off on this deal."?></p>
     </div>
     <div>
         <h3>Second Deal</h3>
         <p><?php echo "Total Price before discount: $" . $d2TotalPriceF ?></p>
         <p><?php echo "Discount Amount: $" . $d2DiscountAmountF ?></p>
+        <p><?php echo "You paid $" . $d2EachF . " for each item." ?></p>
         <p><?php echo "You got " . $d2PercentF . "% off on this deal."?></p>
     </div>
 </body>
