70dcb404ea9a04973db6d668f9822678086d041f
Julio Quevedo <juliovquevedo@gmail.com>
2019-02-11 14:23:26 -0500

    Formatted output

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ $d1Item2 = filter_input(INPUT_POST, 'getthisdeal');
 $d1Discount = filter_input(INPUT_POST, 'discount');
 
 $d1TotalPrice = $d1Item1Price * $d1Item1 + $d1Item2Price * $d1Item2;
+$d1TotalPriceF = number_format($d1TotalPrice, 2);
 $d1DiscountAmount = $d1Item2Price * $d1Item2 * $d1Discount / 100;
+$d1DiscountAmountF = number_format($d1DiscountAmount, 2);
 $d1Percent = $d1DiscountAmount / $d1TotalPrice * 100;
 $d1PercentF = number_format($d1Percent, 2);
 ?>
@@ -18,8 +20,8 @@ $d1PercentF = number_format($d1Percent, 2);
         <title>Compare Deals</title>
     </head>
     <body>
-        <p><?php echo "Total Price before discount: " . $d1TotalPrice ?></p>
-        <p><?php echo "Discount Amount: " . $d1DiscountAmount ?></p>
-        <p><?php echo "You got " . $d1PercentF . " percent off on this deal."?></p>
+        <p><?php echo "Total Price before discount: $" . $d1TotalPriceF ?></p>
+        <p><?php echo "Discount Amount: $" . $d1DiscountAmountF ?></p>
+        <p><?php echo "You got " . $d1PercentF . " % off on this deal."?></p>
     </body>
 </html>
